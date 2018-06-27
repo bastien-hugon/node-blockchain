da5f78c56a5b21b9f11f689039638f16365cf5ce
Bastien Hugon <bastien.hugon@epitech.eu>
2018-06-27 15:40:39 +0200

    This blockchain is working but that's not usefull

---
+++ main.js
@@ -0,0 +1,65 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+
+	constructor(data) {
+		this.index = 0;
+		this.timestamp = Date.now();
+		this.data = data;
+		this.previousHash = "0";
+		this.hash = this.calculateHash();
+		this.nonce = 0;
+	}
+
+	calculateHash() {
+		return SHA256(this.index + this.previousHash + this.timestamp + this.data + this.nonce).toString();
+	}
+
+	mineBlock(difficulty) {
+
+	}
+}
+
+class Blockchain {
+
+	constructor() {
+		this.chain = [this.createGenesis()];
+	}
+
+	createGenesis() {
+		return new Block(0);
+	}
+
+	latestBlock() {
+		return this.chain[this.chain.length - 1];
+	}
+
+	addBlock(newBlock){
+		newBlock.previousHash = this.latestBlock().hash;
+		newBlock.hash = newBlock.calculateHash();
+		this.chain.push(newBlock);
+	}
+
+	checkValid() {
+		for(let i = 1; i < this.chain.length; i++) {
+			const currentBlock = this.chain[i];
+			const previousBlock = this.chain[i - 1];
+
+			if (currentBlock.hash !== currentBlock.calculateHash()) {
+				return false;
+			}
+
+			if (currentBlock.previousHash !== previousBlock.hash) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
+
+let jsChain = new Blockchain();
+jsChain.addBlock(new Block({amount: 5}));
+jsChain.addBlock(new Block({amount: 10}));
+
+console.log(JSON.stringify(jsChain, null, 4));
+console.log("Is blockchain valid? " + jsChain.checkValid());
